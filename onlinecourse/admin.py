0f6c490553a268455f7dcb7a8f2190eed13e6b42
kcashique <ashiquekc97@gmail.com>
2023-04-20 19:41:35 -0400

    Build Course Exam Related Models and Templates

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('course', 'qustion_text', 'grade')
+    list_filter = ['course']
+    search_fields = ['course', 'grade']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'choice_text', 'is_correct')
+    list_filter = ['question']
+    search_fields = ['question', 'choice_text']
 
 
 class LessonInline(admin.StackedInline):
@@ -24,6 +45,8 @@ class LessonAdmin(admin.ModelAdmin):
 
 # <HINT> Register Question and Choice models here
 
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
